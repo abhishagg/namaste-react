b3813d989346c94bd2ad2edc58f6814c25ff3c1e
Abhishek <abhishek.bansal@appinventiv.com>
2023-01-13 16:24:00 +0530

    functional-components

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-let heading1 = React.createElement(
-    "h1",
-    {
-        id: "title"
-    },
-    "Namaste React!"
-);
-let heading2 = React.createElement(
-    "h2",
-    {
-        id: "title"
-    },
-    "Namaste React!"
-);
-let container = React.createElement(
-    "div",
-    {
-        id: "container"
-    },
-    [heading1, heading2]
-);
+// react element
+const title = (
+    <h1 id="title" key="h1">
+        Namaste React! title
+    </h1>
+)
+
+// react component 
+
+const Heading = () => (
+    <h1 id="title" key="h2">
+        Namaste React! Heading
+    </h1>
+)
+
+
+let HeaderComponent = () => (
+    <div>
+        {title}
+        <Heading />
+        <h2>Heading from functional component</h2>
+        <h3>This is a h3 tag</h3>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container)
+root.render(<HeaderComponent />)
